cdb5eadb9017813678108bc1801c1758fb51496b
Julian <julian.montoya@qentek.com>
2016-11-15 13:25:04 -0500

    Filter to image size

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 $(function() {
 	$( ".input--search" ).keypress(function() {
 		var searchWord = $( ".input--search" ).val();
-	  	console.log( "Handler for .keypress() called. "+searchWord );
 	  	searchImages(searchWord);
 	});
 });
 
 function searchImages(searchWord){
-	var url_pixabay = "https://pixabay.com/api/?key=3768083-03c9b9f4d602401c878f9a1ab&q="+searchWord+"&image_type=photo";
+	var API_KEY = '3768083-03c9b9f4d602401c878f9a1ab';
+	var url_pixabay = "https://pixabay.com/api/?key="+API_KEY+"&q="+searchWord+"&image_type=photo&min_width=300&min_height=300";
   	$.ajax({
 	  url: url_pixabay,
 	}).done(function(data) {
-	   //console.log(data.hits);
 	   var html_result ="";
 		jQuery.each(data.hits, function() {
-		  console.log(this);
-		  html_result = html_result+'<img src="'+this.previewURL+'" class="image image--result">';
+			
+			var height= this.imageHeight;
+			var width= this.imageWidth;
+			console.log(" h"+height+" w"+width);
+			if(width>height){
+			  html_result = html_result+'<div class="result">'
+			  							+'<a href="'+this.pageURL+'" target="_blank">'
+			  								+'<img src="'+this.previewURL+'" class="image image--result">'
+			  							+'</a>'
+			  								+'<div class="detail--result">'
+				  								+this.likes+' <img src="img/like.png" class="icon"> '
+				  								+this.comments+' <img src="img/comment.png" class="icon"> '
+				  								+this.favorites+' <img src="img/fav.png" class="icon"> '
+				  								+'</div>'
+			  							+'</div>';
+			}
 		});
-	  //var html_result =;
 
 	  $('.noResultados').html("Numero de resultados: "+data.total);
 	  $('.results').html(html_result);
